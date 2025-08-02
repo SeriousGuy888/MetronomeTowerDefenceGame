ffee7d51ac9c2b84d7825e893bacbcda6404fb03
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-08-02 15:25:32 -0400

    make enemies store what lane they're in

--- global_scripts/enums.gd
+++ global_scripts/enums.gd
@@ -5,3 +5,14 @@ enum GameState {
 	BUILD = 1, # time to build and upgrade setup. no monsters approach.
 	GAME_OVER = 2,
 }
+
+enum Lane {
+	TOP = 270,
+	TOP_RIGHT = 315,
+	RIGHT = 0,
+	BOTTOM_RIGHT = 45,
+	BOTTOM = 90,
+	BOTTOM_LEFT = 135,
+	LEFT = 180,
+	TOP_LEFT = 225,
+}
--- scenes/monster_spawner.gd
+++ scenes/monster_spawner.gd
@@ -10,16 +10,18 @@ func _ready():
 
 func _on_request_spawn_enemy(enemy_scene: PackedScene, wave_index: int = -1):
 	# 1. Pick a random angle from your list of defined lanes.
-	var chosen_angle_deg = lane_angles_degrees.pick_random()
+	var chosen_lane_key = Enums.Lane.keys().pick_random()
+	var chosen_angle_deg = Enums.Lane.get(chosen_lane_key)
 	# 2. Convert the chosen angle from degrees to radians for calculations.
 	var chosen_angle_rad = deg_to_rad(chosen_angle_deg)
 	# 3. Calculate the spawn position using the angle and radius.
 	var spawn_position = Vector2.from_angle(chosen_angle_rad) * spawn_radius
 	# 4. Create an instance of the enemy scene.
-	var enemy_instance = enemy_scene.instantiate()
+	var enemy_instance: Enemy = enemy_scene.instantiate()
 	# 5. Set the enemy's global position to the calculated spawn point.
 	enemy_instance.global_position = spawn_position
-	# mark the enemy's wave membership
+	# mark the enemy's wave and lane membership
 	enemy_instance.member_of_wave = wave_index
+	enemy_instance.lane = chosen_angle_deg
 	# 6. Add the new enemy to the designated container node.
 	enemy_container.add_child(enemy_instance)
