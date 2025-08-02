73b94f07f585ded4cba3c09c7699979d82f49988
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-08-02 15:50:24 -0400

    homing ring projectiles prioritise enemies in own lane

--- scenes/projectiles/homing_ring.gd
+++ scenes/projectiles/homing_ring.gd
@@ -2,11 +2,16 @@ class_name HomingRing extends Projectile
 
 @export var targeted_enemy: Enemy = null
 
+var lane: Enums.Lane
+
 var direction: Vector2:
 	get: return direction
 	set(value): direction = value.normalized()
 
 var tick = 0
+
+func _ready() -> void:
+	direction = Vector2.from_angle(deg_to_rad(lane))
 
 func _process(delta) -> void:
 	super(delta)
@@ -15,7 +20,11 @@ func _process(delta) -> void:
 	elif tick % 20 == 0:
 		var enemies = PlaythroughManager.get_tree().get_nodes_in_group("enemies")
 		if !enemies.is_empty():
-			targeted_enemy = enemies.pick_random()
+			var enemies_in_own_lane = enemies.filter(func(enemy: Enemy): return enemy.lane == lane)
+			if !enemies_in_own_lane.is_empty():
+				targeted_enemy = enemies_in_own_lane.pick_random()
+			else:
+				targeted_enemy = enemies.pick_random()
 	
 	position += direction * 250 * delta
 	tick += 1
--- scenes/turrets/types/triangle_turret.gd
+++ scenes/turrets/types/triangle_turret.gd
@@ -3,7 +3,7 @@ extends TurretType
 
 func _setup_projectile_instance(lane: Enums.Lane) -> HomingRing:
 	var projectile: HomingRing = projectile_scene.instantiate()
-	projectile.direction = Vector2.from_angle(deg_to_rad(lane))
+	projectile.lane = lane
 	projectile.damage = 0.25
 	
 	return projectile
